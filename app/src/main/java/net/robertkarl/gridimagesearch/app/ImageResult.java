81e41c4fbda80c79c9b4eb128bb07d67ea0480b7
Robert Karl <robertkarljr@gmail.com>
2014-06-13 19:19:25 -0700

    Pulling some results from the server

---
+++ app/src/main/java/net/robertkarl/gridimagesearch/app/ImageResult.java
@@ -0,0 +1,47 @@
+package net.robertkarl.gridimagesearch.app;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class ImageResult {
+
+    private String fullURL;
+    private String thumbURL;
+
+    public ImageResult(JSONObject json) {
+        try {
+            this.fullURL = json.getString("url");
+            this.thumbURL = json.getString("tbUrl");
+        }
+        catch (JSONException e) {
+            // ignore
+        }
+    }
+
+    public String getThumbURL() {
+        return thumbURL;
+    }
+
+    public String toString() {
+        return "ImageResult " + thumbURL;
+    }
+
+    public String getFullURL() {
+        return fullURL;
+    }
+    public static ArrayList<? extends ImageResult> fromJSONArray (JSONArray array) {
+        ArrayList <ImageResult> results = new ArrayList<ImageResult>();
+        for (int i = 0; i < array.length(); i++) {
+            try {
+                results.add(new ImageResult(array.getJSONObject(i)));
+            }
+            catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return results;
+    }
+}
--- app/src/main/java/net/robertkarl/gridimagesearch/app/SearchActivity.java
+++ app/src/main/java/net/robertkarl/gridimagesearch/app/SearchActivity.java
@@ -1,22 +1,43 @@
 package net.robertkarl.gridimagesearch.app;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.GridView;
+import android.widget.Toast;
 
-import com.loopj.android.image.SmartImage;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 public class SearchActivity extends Activity {
+
+    private EditText etQuery;
+    private Button btnSearch;
+    private GridView gvResults;
+
+    ArrayList<ImageResult> imageResults = new ArrayList<ImageResult>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        SmartImage image;
-    }
+        etQuery = (EditText)findViewById(R.id.etQuery);
+        btnSearch = (Button)findViewById(R.id.btnSearch);
+        gvResults = (GridView)findViewById(R.id.gvResults);
 
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -32,4 +53,29 @@ public class SearchActivity extends Activity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public void onSearchClicked(View v) {
+        String query = etQuery.getText().toString();
+        Toast.makeText(this,String.format("Searching for %s", query), Toast.LENGTH_LONG).show();
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get("http://ajax.googleapis.com/ajax/services/search/images?rsz=8&start=" + 0 + "&v=1.0&q=" + Uri.encode(query),
+                new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(JSONObject response) {
+                        super.onSuccess(response);
+                        JSONArray imageJsonResults = null;
+                        try {
+                            imageJsonResults = response.getJSONObject("responseData").getJSONArray("results");
+                            imageResults.clear();
+                            imageResults.addAll(ImageResult.fromJSONArray(imageJsonResults));
+                            Log.d("DEBUG", imageResults.toString());
+                        }
+                        catch (JSONException e) {
+                            // ignore
+                        }
+                    }
+                });
+    }
+
 }
